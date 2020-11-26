9fad886076a1a00872aa2b4d7105d930ba847c19
Asif Mohammad <asifratul45@gmail.com>
2020-11-26 15:20:13 +0600

    localhost added

--- src/components/Favourites/Favourites.js
+++ src/components/Favourites/Favourites.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from '../Header/Header';
+import SideNav from '../SideNav/SideNav';
+import SingleCountry from '../SingleCountry/SingleCountry';
 
 const Favourites = () => {
     return (
-        <div>
-            This is favourites
+        <div className="favourites">
+            <Header />
+            <SideNav />
+
+            <div className="left-margin country-section">
+                <h1 className="headline">Favourites - {}</h1>
+                {
+                    //favouriteList.map(country => <SingleCountry key={country.alpha2Code} country={country} />)
+                }
+            </div>
         </div>
     );
 };
